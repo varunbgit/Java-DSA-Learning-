589082b36820a289039f73aabe3ca5efe0de1078
Varun Bansal <varunbansal@Varuns-MacBook-Air.local>
2022-05-29 17:33:02 +0530

    IMP point of Augnoistic Binary search

--- FindElementInPeakArray.java
+++ FindElementInPeakArray.java
@@ -10,15 +10,17 @@ public class FindElementInPeakArray {
         //then keeping it as the end point
         //because that gives us the ideal Sorted Array for BinarySearch
         //do a Binary Search to find the Particular Element in the left part (As we need the minimum index in the array)
-       int[] arr = {1,2,3,4,5,3,1};
+       int[] arr = {1,2,4,5,3,1};
        int target= 3;
         System.out.println("Element present at index "+IndexSearch(arr,target));
     }
 
+    //One important Point you might have missed
+    //What if the element is not present in Ascending Part of Array but is Present in Desending part of array
+
     public static int IndexSearch(int [] arr, int target) {
         int start = 0;
         int end = Peakelement(arr);
-
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
@@ -28,7 +30,21 @@ public class FindElementInPeakArray {
                 end = mid - 1;
             } else {
                 return mid;
+            }
+        }
+        //B Search in Descending Array
+        start = end;
+        end =arr.length -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
+            if (arr[mid] < target) {
+
+                end = mid - 1;
+            } else if (arr[mid] > target) {
+                start = mid + 1;
+            } else {
+                return mid;
             }
         }
         return -1;
