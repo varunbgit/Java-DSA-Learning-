6081a06f22517ec357acddfc607e5ad0111e3e76
varunbgit <vbansalevents01@gmail.com>
2022-06-01 16:46:48 +0530

    Find index of Rotated Pivot

--- RotatedCount.java
+++ RotatedCount.java
@@ -1,2 +1,38 @@
-package com.varun;public class RotatedCount {
+package com.varun;
+
+public class RotatedCount {
+    public static void main(String[] args) {
+        //find the pivot(Greatest element
+        //and Ans is index of Pivot elemement +1
+
+        int arr[] = {4,6,7,0,1,2,3};
+        int ans = CheckPivot(arr)  + 1 ;
+        System.out.println("Array is Rotated by index "+ans);
+
+    }
+
+    private static int CheckPivot(int[] arr) {
+        int start=0;
+        int end =arr.length-1;
+        while(start<=end ){
+            int mid = start + (end - start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if (mid>start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[start]<arr[mid]){
+                end = mid -1;
+            }
+            else{
+                start=mid+1;
+            }
+
+        }
+
+        return -1;
+    }
 }
+
+
