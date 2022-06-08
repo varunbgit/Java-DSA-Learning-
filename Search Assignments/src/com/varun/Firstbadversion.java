42a3a388ee33349a56504932e909c2584ccde178
varunbgit <vbansalevents01@gmail.com>
2022-06-08 16:37:35 +0530

    Basic Problem on loops

---
+++ Search Assignments/src/com/varun/Firstbadversion.java
@@ -0,0 +1,34 @@
+package com.varun;
+// 278. First Bad Version
+
+/*You are given an API bool isBadVersion(version) which returns whether version is bad.
+Implement a function to find the first bad version.
+        You should minimize the number of calls to the API.
+*/
+public class Firstbadversion {
+    public static void main(String[] args) {
+        int n=5;
+        int isbad = firstBadVersion(n);
+
+
+    }
+    public static int firstBadVersion(int n) {
+       // int ans=0;
+        int start =0 ;
+        int end = n;
+
+        while (start <= end){
+            int mid = start + (end-start)/2;
+            if(isBadVersion(mid)){
+                if(isBadVersion(mid-1)){
+                    end = mid-1;
+                }else{
+                    return mid;
+                }
+            }else{
+                start = mid+1;
+            }
+        }
+        return  -1;
+    }
+}
