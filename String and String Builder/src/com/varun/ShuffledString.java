b90786ba281d17a1a12d30ee53aa05c0aabe0755
varunbgit <vbansalevents01@gmail.com>
2022-07-04 23:06:38 +0530

    Shuffle Array v2 more Optimized

--- String and String Builder/src/com/varun/ShuffledString.java
+++ String and String Builder/src/com/varun/ShuffledString.java
@@ -1,5 +1,7 @@
 package com.varun;
 
+import java.util.Arrays;
+
 public class ShuffledString {
     public static void main(String[] args) {
         String s = "codeleet";
@@ -8,17 +10,30 @@ public class ShuffledString {
         System.out.println(ans);
     }
 
-    public static String restoreString(String s, int[] indices) {
-        int num =0;
-        StringBuilder ans = new StringBuilder("");
-        while(num<s.length()){
-            for (int i = 0; i < indices.length ; i++) {
-                if(indices[i] == num){
-                    ans.append(s.charAt(i));
-                    num++;
-                }
-            }
-        }
-        return ans.toString();
+//    public static String restoreString(String s, int[] indices) {
+//        int num =0;
+//        StringBuilder ans = new StringBuilder("");
+//        while(num<s.length()){
+//            for (int i = 0; i < indices.length ; i++) {
+//                if(indices[i] == num){
+//                    ans.append(s.charAt(i));
+//                    num++;
+//                }
+//            }
+//        }
+//        return ans.toString();
+//    }
+
+
+    //Approach 2
+    //more optimized
+
+public static String restoreString(String s, int[] indices) {
+        char[] ans = new char [indices.length];
+   // StringBuilder finalans = new StringBuilder("");
+    for (int i = 0; i < indices.length; i++) {
+        ans[indices[i]] = s.charAt(i);
     }
+    return new String(ans);
 }
+    }
