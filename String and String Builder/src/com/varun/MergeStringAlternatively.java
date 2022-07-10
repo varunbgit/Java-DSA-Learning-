18fdb999b8a280a6d9b20c5ac08d0850db8df2b9
varunbgit <vbansalevents01@gmail.com>
2022-07-10 23:32:25 +0530

    Merging string Alternatively

--- String and String Builder/src/com/varun/MergeStringAlternatively.java
+++ String and String Builder/src/com/varun/MergeStringAlternatively.java
@@ -9,6 +9,21 @@ public class MergeStringAlternatively {
         System.out.println(ans);
     }
     public static String mergeAlternately(String word1, String word2) {
+        StringBuilder ans = new StringBuilder();
+        int index =0 ;
+
+        int i ;
+        for ( i = 0; i <word1.length() && i<word2.length() ; i++) {
+            ans.append(word1.charAt(i));
+            ans.append(word2.charAt(i));
+        }
+        if(word1.length()>word2.length()){
+            ans.append(word1.substring(i+1)); // if only one argument is passed then it takes string from index passes to last
+        }
+        if(word1.length()<word2.length()){
+            ans.append(word2.substring(i+1));// if only one argument is passed then it takes string from index passes to last
+        }
+        return ans.toString();
 
     }
 }
