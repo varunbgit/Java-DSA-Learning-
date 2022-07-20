8add9330c109db7a25878ff8dc9f85d3d03d68a4
varunbgit <vbansalevents01@gmail.com>
2022-07-20 23:36:52 +0530

    Print sum of numbers till n using Recursion

--- Recursion/src/com/varun/SumNum.java
+++ Recursion/src/com/varun/SumNum.java
@@ -2,6 +2,15 @@ package com.varun;
 
 public class SumNum {
     public static void main(String[] args) {
+        int n= 5;
+        int ans = sum(n);
+        System.out.println(ans);
+    }
 
+    private static int sum(int n) {
+        if(n==1){
+            return 1;
+        }
+        return n + sum(n-1);
     }
 }
