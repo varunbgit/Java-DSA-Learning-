7e10aa43920043d35a447de1a9ca94ccc6e797af
varunbgit <vbansalevents01@gmail.com>
2022-07-20 23:46:52 +0530

    find product of digits using Recursion

---
+++ Recursion/src/com/varun/productofDigits.java
@@ -0,0 +1,16 @@
+package com.varun;
+
+public class productofDigits {
+    public static void main(String[] args) {
+        int n = 1345 ;
+        int ans = sum(n);
+        System.out.println(ans);
+    }
+
+    private static int sum(int n) {
+        if(n%10 == n){
+            return n;
+        }
+        return n%10 * sum(n/10);
+    }
+}
