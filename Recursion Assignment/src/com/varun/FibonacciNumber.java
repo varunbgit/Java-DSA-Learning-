0b81b4160712ea1e70b11c58f35551ff94332f99
varunbgit <vbansalevents01@gmail.com>
2022-07-28 11:53:04 +0530

    Brute force approach for Fibonacci series

---
+++ Recursion Assignment/src/com/varun/FibonacciNumber.java
@@ -0,0 +1,18 @@
+package com.varun;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        int n = 2;
+        int ans = fibo(n);
+        System.out.println(ans);
+    }
+
+    private static int fibo(int n) {
+        if(n<=1){
+            return n;
+
+        }
+
+        return fibo(n-1)+ fibo(n-2);
+    }
+}
