5e47de192591d609554bbac7a39737706e1ebebf
varunbgit <vbansalevents01@gmail.com>
2022-08-03 22:46:09 +0530

    adding a practice random code

---
+++ Recursion Assignment/src/com/varun/FindLenghtofStringusingRecursion.java
@@ -0,0 +1,24 @@
+package com.varun;
+//https://www.geeksforgeeks.org/program-for-length-of-a-string-using-recursion
+
+public class FindLenghtofStringusingRecursion {
+    public static void main(String[] args) {
+        String s  = "Varun";
+        int ans = recLen(s);
+        System.out.println(ans);
+    }
+
+    private static int recLen(String s) {
+
+        if(s == null){
+            return 0;
+        }
+        StringBuilder nstr = new StringBuilder();
+       if(s.length() > 1){
+           nstr.append(s,0,s.length()-1);
+       }
+        String newS = nstr.toString();
+
+        return recLen(newS) +1;
+    }
+}
--- Recursion Assignment/src/com/varun/Main.java
+++ Recursion Assignment/src/com/varun/Main.java
@@ -3,11 +3,30 @@ package com.varun;
 import java.util.Arrays;
 
 public class Main {
+    public static int findIndex(){
+        int arr[] ={5,7,8,9,10,16,18};
+        int target = 10;
+        int len=arr.length;
+        int start=0;
+        int end=len-1;
+
+        System.out.println("reached here ");
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==target){
+                return mid;
+            } else if(arr[mid]<target){
+                start=mid+1;
+            } else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
 	// write your code here
-
-        int [] arr = new  int [10];
-        System.out.println(Arrays.toString(arr));
+int ans = findIndex();
+        System.out.println(ans);
     }
 }
