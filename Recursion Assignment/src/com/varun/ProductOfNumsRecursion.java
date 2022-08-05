4e2ffa558b54655c81a33c1e1750210a8a98e056
varunbgit <vbansalevents01@gmail.com>
2022-08-05 13:30:07 +0530

    Product of numbers using Recursion

---
+++ Recursion Assignment/src/com/varun/ProductOfNumsRecursion.java
@@ -0,0 +1,26 @@
+package com.varun;
+
+public class ProductOfNumsRecursion {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 55;
+        int ans = product(x,y);
+        System.out.println(ans);
+    }
+
+    public static int product(int x, int y) {
+        // Assuming x is greater and  y is smaller
+
+        if(x > y){
+            int temp = x;
+            x= y;
+            y = temp;
+        }
+        if(x==0 || y==0){
+            return 0;
+        }
+
+        return x + product(x,y-1);
+
+    }
+}
