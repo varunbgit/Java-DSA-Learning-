eea8f0e38c851e6908742dd045a8bf3fab92899c
varunbgit <vbansalevents01@gmail.com>
2022-08-10 09:50:52 +0530

    tried pow of 3 question

---
+++ Recursion Assignment/src/com/varun/powof3.java
@@ -0,0 +1,22 @@
+package com.varun;
+
+//https://leetcode.com/problems/power-of-three/
+
+public class powof3 {
+    public static void main(String[] args) {
+        int n = 0;
+        boolean ans  = isPowerOfThree(n);
+        System.out.println(ans);
+
+    }
+    public static boolean isPowerOfThree(int n) {
+        if(n>0){
+
+        }
+        return  false;
+    }
+}
+3 = 11
+9 = 101
+27 = 11011;
+
