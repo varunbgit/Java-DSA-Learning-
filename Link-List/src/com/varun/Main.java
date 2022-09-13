b30f9bbdccb9b099b7e7ae4a68b6c925d8c43fc7
varunbgit <vbansalevents01@gmail.com>
2022-09-14 00:07:54 +0530

    Starting with the Linked List

---
+++ Link-List/src/com/varun/Main.java
@@ -0,0 +1,16 @@
+package com.varun;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        LinkList list = new LinkList();
+        list.insertAtFirst(10);
+        list.insertAtFirst(9);
+        list.insertAtFirst(8);
+        list.insertAtFirst(7);
+
+        list.displayList();
+    }
+}
