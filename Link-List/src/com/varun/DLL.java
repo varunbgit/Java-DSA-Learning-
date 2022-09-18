93e164c9a15624b3e568a9c0283a00ee921ddc4b
varunbgit <vbansalevents01@gmail.com>
2022-09-18 13:17:54 +0530

    Added the code for insertion at first place in doublly Linked lIst

--- Link-List/src/com/varun/DLL.java
+++ Link-List/src/com/varun/DLL.java
@@ -30,6 +30,21 @@ public class DLL {
 
         public void  insertFirst(int value){
             Node node = new Node(value);
+            //if first node in list
+            if(head == null){
+                node.next = null;
+                node.prev = null;
+                head = node;
+
+            }
+            else {
+                // if this is not first node;
+                head.prev = node;
+                node.next = head;
+                node.prev = null;
+                head = node;
+            }
+            size++;
 
         }
         public void display(){
