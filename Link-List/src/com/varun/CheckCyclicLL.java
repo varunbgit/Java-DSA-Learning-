dfb04fb0cf71b57b2ef36b61a455a670173165ac
varunbgit <vbansalevents01@gmail.com>
2022-09-28 22:37:26 +0530

    CYCLE CHECK IN LINK LIST

--- Link-List/src/com/varun/CheckCyclicLL.java
+++ Link-List/src/com/varun/CheckCyclicLL.java
@@ -1,2 +1,31 @@
-package com.varun;public class CheckCyclicLL {
+package com.varun;
+
+public class CheckCyclicLL {
+
+}
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while( fast!=null  && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast ){
+                return true;
+            }
+        }
+        return false;
+    }
 }
