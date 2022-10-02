374c6b5d7d3e8dae9b50e1a062368c191eb9ab87
varunbgit <vbansalevents01@gmail.com>
2022-10-02 15:21:37 +0530

    Added the working  code of returing the first node of linked list

--- Link-List/src/com/varun/LL_Cycle_length.java
+++ Link-List/src/com/varun/LL_Cycle_length.java
@@ -1,27 +1,27 @@
-package com.varun;
-
-public class LL_Cycle_length {
-//code to check if cycle exists in LL
-
-    public int  hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while( fast!=null  && fast.next !=null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(slow == fast ){
-                //here it is confirmed that cycle exists
-             int len = 0;
-             do{
-                 slow = slow.next;
-                 len++;
-             }
-             while(slow != fast);
-                 return count ;
-            }
-        }
-
-        return -1; // implies no cycle exits
-    }
-}
+//package com.varun;
+//
+//public class LL_Cycle_length {
+////code to check if cycle exists in LL
+//
+//    public int  hasCycle(ListNode head) {
+//        ListNode slow = head;
+//        ListNode fast = head;
+//
+//        while( fast!=null  && fast.next !=null){
+//            fast = fast.next.next;
+//            slow = slow.next;
+//            if(slow == fast ){
+//                //here it is confirmed that cycle exists
+//             int len = 0;
+//             do{
+//                 slow = slow.next;
+//                 len++;
+//             }
+//             while(slow != fast);
+//                 return count ;
+//            }
+//        }
+//
+//        return -1; // implies no cycle exits
+//    }
+//}
--- Link-List/src/com/varun/ReturnTheCylicNode.java
+++ Link-List/src/com/varun/ReturnTheCylicNode.java
@@ -1,46 +1,56 @@
-package com.varun;
+//https://leetcode.com/problems/linked-list-cycle-ii/submissions/
+// /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        //calculate the lenght of cycle
+        ListNode slow = head;
+        ListNode fast = head;
+        int len  = 0;
+        while(fast!= null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
 
-public class ReturnTheCylicNode {
+            if(slow == fast){
+                // cycle exits
+                ListNode temp = slow;
+                do{
+                    temp  = temp.next;
+                    len = len + 1;
 
-    public class Solution {
-        public ListNode detectCycle(ListNode head) {
-            //calculate the lenght of cycle
-            ListNode slow = head;
-            ListNode fast = head;
-            int len  = 0;
-            while(fast!= null && fast.next != null){
-                slow = slow.next;
-                fast = fast.next.next;
-                if(slow == fast){
-                    // cycle exits
-                    ListNode temp = slow;
-                    do{
-                        temp  = temp.next;
-                        len = len + 1;
-                        System.out.println(len);
-                    }
-                    while(slow!=temp);
                 }
+                while(temp!=slow);
+                break;
             }
-
-
-//             fast = head;
-//             slow = head;
-//             for(int i=0;i<len;i++){
-//                  slow= slow.next;
-//             }
-
-//             if(slow == fast){
-//                 return slow;
-//             }
-
-//             // ListNode t = head ;
-
-//             slow= slow.next;
-//             fast = fast.next;
-
-            return null;
-
         }
+
+        System.out.println(len);
+
+        if(len <=0){
+            return null;
+        }
+        fast = head;
+        slow = head;
+        for(int i=0;i<len;i++){
+            slow= slow.next;
+        }
+
+        while(slow!=fast){
+            slow= slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+
+
     }
 }
