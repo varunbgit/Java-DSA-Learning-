15ca98a2f6de262d2595ef9523ad850e9d4560c9
varunbgit <vbansalevents01@gmail.com>
2022-10-04 01:27:18 +0530

    Reverse LinkedList

---
+++ Link-List/src/com/varun/ReverseLinkedList.java
@@ -0,0 +1,50 @@
+//package com.varun;
+//
+//206 Rverse Linked List
+
+
+
+//public class ReverseLinkedList {
+//    /**
+//     * Definition for singly-linked list.
+//     * public class ListNode {
+//     *     int val;
+//     *     ListNode next;
+//     *     ListNode() {}
+//     *     ListNode(int val) { this.val = val; }
+//     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//     * }
+//     */
+//    class Solution {
+//        public ListNode reverseList(ListNode head) {
+//            if(head == null ){
+//                return null;
+//            }
+//            //insertAtfirst
+//            ListNode anshead = new ListNode();
+//            // ListNode anstemp=  new ListNode();
+//            // anstemp = anshead;
+//            int size=0;
+//
+//
+//            while(head!=null){
+//                if(size == 0){
+//                    anshead.val = head.val;
+//                    head = head.next;
+//                    anshead.next= null;
+//
+//
+//                }else{
+//                    ListNode temp = new ListNode(head.val,anshead);
+//                    // temp.val = head.value;
+//                    //temp.next= anshead;
+//                    anshead = temp;
+//                    head = head.next;
+//                }
+//                size++;
+//            }
+//
+//            return anshead;
+//        }
+//    }
+//}
