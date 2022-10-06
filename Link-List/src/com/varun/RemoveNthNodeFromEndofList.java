5bcb2a3e9effb4ed34eace096a25181f5558f0db
varunbgit <vbansalevents01@gmail.com>
2022-10-06 23:59:01 +0530

    remove the nth node from last optimized code

--- Link-List/src/com/varun/RemoveNthNodeFromEndofList.java
+++ Link-List/src/com/varun/RemoveNthNodeFromEndofList.java
@@ -35,3 +35,27 @@
 //        }
 //    }
 //}
+
+//approach2
+//
+//class Solution {
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        ListNode start = new ListNode(0);
+//        start.next = head;
+//        ListNode slow = start;
+//        ListNode fast = start;
+//
+//        for(int i=0;i<n;i++){
+//            fast = fast.next;
+//        }
+//        // System.out.println(fast.val);
+//
+//        while(fast.next!=null){
+//            slow = slow.next;
+//            fast = fast.next;
+//        }
+//        //slow is at the required node
+//        slow.next = slow.next.next;
+//        return start.next;
+//    }
+//}
