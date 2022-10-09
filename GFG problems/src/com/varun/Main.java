558d39c5eb50e1ba86cc4b2f1793b788b378bd24
varunbgit <vbansalevents01@gmail.com>
2022-10-09 22:45:39 +0530

    Check if both arrays are same GS

---
+++ GFG problems/src/com/varun/Main.java
@@ -0,0 +1,28 @@
+//package com.varun;
+//
+//https://practice.geeksforgeeks.org/problems/check-if-two-arrays-are-equal-or-not3847/1?page=1&category[]=Sorting&curated[]=1&sortBy=submissions
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//	// write your code here
+//        class Solution{
+//            //Function to check if two arrays are equal or not.
+//            public static boolean check(long A[],long B[],int N)
+//            {
+//                //Your code here
+//
+//                Arrays.sort(A);
+//                Arrays.sort(B);
+//
+//                for(int i=0;i<A.length;i++){
+//                    if(A[i]!=B[i]){
+//                        return false;
+//                    }
+//                }
+//                return true;
+//
+//            }
+//        }
+//    }
+//}
