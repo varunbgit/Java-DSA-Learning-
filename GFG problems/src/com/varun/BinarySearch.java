eb9bea3fb091fad9950a0ae7fe7a5ce0d4731762
varunbgit <vbansalevents01@gmail.com>
2022-10-09 22:53:15 +0530

    Binary Search code

---
+++ GFG problems/src/com/varun/BinarySearch.java
@@ -0,0 +1,27 @@
+//package com.varun;
+////https://practice.geeksforgeeks.org/problems/binary-search-1587115620/1?page=1&curated[]=1&sortBy=submissions
+//public class BinarySearch {
+//
+//    class Solution {
+//        int binarysearch(int arr[], int n, int k) {
+//            // code here
+//            int start = 0;
+//            int end = arr.length-1;
+//
+//            while(start<=end){
+//                int mid=(start+end)/2;
+//                if(arr[mid]==k){
+//                    return mid;
+//                }
+//                if(arr[mid]>k){
+//                    end = mid -1;
+//                }
+//                else{
+//                    start = mid+1;
+//                }
+//            }
+//            return -1;
+//        }
+//    }
+//
+//}
