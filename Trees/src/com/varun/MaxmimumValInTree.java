c60526cd14c287e248ae3538f5962fa64c71128c
varunbgit <vbansalevents01@gmail.com>
2022-10-13 18:36:31 +0530

    Maximum Value in Tree V2

--- Trees/src/com/varun/MaxmimumValInTree.java
+++ Trees/src/com/varun/MaxmimumValInTree.java
@@ -3,8 +3,10 @@ package com.varun;
 public class MaxmimumValInTree {
     int max = 0;
     Maxval(root,max);
-    public MaxVal(TreeNode root,int max){
-
+    public int MaxVal(TreeNode root,int max){
+        if(root==null){
+            return Integer.MIN_VALUE;
+        }
         max = Math.max(root.max,Math.max(MaxVal(root.left),MaxVal(root.right)));
         return ans;
     }
