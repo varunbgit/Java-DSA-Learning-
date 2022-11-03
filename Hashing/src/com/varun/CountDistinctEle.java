f602fa39fc32ac12af03c5fc82670a493c7d6f40
varunbgit <vbansalevents01@gmail.com>
2022-11-03 21:49:02 +0530

    Count Distinct Element V2 more Improvised

--- Hashing/src/com/varun/CountDistinctEle.java
+++ Hashing/src/com/varun/CountDistinctEle.java
@@ -1,25 +1,32 @@
 package com.varun;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class CountDistinctEle {
 
     public static void main(String[] args) {
-        int[] arr = {1 ,1 ,2 ,2 ,3 ,3 ,4 ,5 ,6 ,7};
+        Integer[] arr = {1 ,1 ,2 ,2 ,3 ,3 ,4 ,5 ,6 ,7};
         int ans = countNonRepeated(arr);
         System.out.println(ans);
     }
-    public static int countNonRepeated(int arr[])
+    public static int countNonRepeated(Integer arr[])
     {
-        // add your code
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i <arr.length ; i++) {
-            set.add(arr[i]);
-        }
+//        // add your code
+//        HashSet<Integer> set = new HashSet<>();
+//        for (int i = 0; i <arr.length ; i++) {
+//            set.add(arr[i]);
+//        }
+//
+//        return set.size();
+
+        // Approach 2 more Improvised
+        HashSet<Integer> set = new HashSet<>(Arrays.asList(arr));
+        // just remember for using it as constructor you must use Wrapper Class Integer instead of int
 
         return set.size();
-
     }
 
 }
