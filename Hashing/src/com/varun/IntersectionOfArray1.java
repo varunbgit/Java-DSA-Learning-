ab7a5d2cad3a2f36d104198832ec9c7e3383fdb7
varunbgit <vbansalevents01@gmail.com>
2022-11-06 20:57:35 +0530

    Code to check the letter missed during transmission

---
+++ Hashing/src/com/varun/IntersectionOfArray1.java
@@ -0,0 +1,15 @@
+//package com.varun;
+////Leetcode : https://leetcode.com/problems/intersection-of-two-arrays/
+//
+//public class IntersectionOfArray1 {
+//    public static void main(String[] args) {
+//        int []nums1 = {1,2,2,1};
+//        int []nums2 = {2,2};
+//        int[] ans = intersection(nums1,nums2);
+//        System.out.println(ans.toString());
+//
+//    }
+//    public static int[] intersection(int[] nums1, int[] nums2) {
+//
+//    }
+//}
--- Hashing/src/com/varun/Main.java
+++ Hashing/src/com/varun/Main.java
@@ -1,8 +1,39 @@
 package com.varun;
 
+import java.util.HashSet;
+
 public class Main {
 
-    public static void main(String[] args) {
+
 	// write your code here
+        public static char lostChar(String stringSent, String stringRec)
+        {
+            char answer =stringSent.charAt(stringSent.length()-1) ;
+// Write your code here
+            HashSet<Character> set  = new HashSet<>();
+            for (int i = 0; i < stringRec.length(); i++) {
+                set.add(stringRec.charAt(i));
+            }
+            //checking the string sent
+
+            for (int i = 0; i <stringSent.length() ; i++) {
+                if(!set.contains(stringSent.charAt(i))){
+                    return stringSent.charAt(i);
+                }
+            }
+
+            return answer;
+        }
+    public static void main(String[] args)
+    {
+ //       Scanner in = new Scanner(System.in);
+// input for stringSent
+        String stringSent = "abcdfjgerj";
+        // input for stringRec
+        String stringRec = "abcdfjger";
+        char result = lostChar(stringSent, stringRec);
+        System.out.print(result);
     }
 }
+
+
