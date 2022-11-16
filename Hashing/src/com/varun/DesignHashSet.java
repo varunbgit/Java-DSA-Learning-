68c88a493f74e377fd137d004bb2e4c674171a48
varunbgit <vbansalevents01@gmail.com>
2022-11-16 22:26:53 +0530

    Design HashSet without using hashset libraries

---
+++ Hashing/src/com/varun/DesignHashSet.java
@@ -0,0 +1,40 @@
+package com.varun;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DesignHashSet {
+    public static void main(String[] args) {
+        String[] str = ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"];
+int []arr = {[], 1, 2, 1, 3, 2, 2, 2, 2}
+    }
+
+
+
+    class MyHashSet {
+List<Integer> list;
+        public MyHashSet() {
+list = new ArrayList<>();
+        }
+
+        public void add(int key) {
+            if(!list.contains(key)){
+                list.add(key);
+            }
+        }
+
+        public void remove(int key) {
+            if(list.contains(key)){
+                list.remove(key);
+            }
+        }
+
+        public boolean contains(int key) {
+            if(list.contains(key)){
+                return true;
+            }
+            return false;
+        }
+    }
+}
