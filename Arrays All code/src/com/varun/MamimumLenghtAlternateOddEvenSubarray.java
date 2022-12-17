8320ce60eedbf41758f3bdd4590f73c6603f605d
varunbgit <vbansalevents01@gmail.com>
2022-12-18 00:27:19 +0530

    Maxmimum length of subArray with alternative even and odds optimized solutions

--- Arrays All code/src/com/varun/MamimumLenghtAlternateOddEvenSubarray.java
+++ Arrays All code/src/com/varun/MamimumLenghtAlternateOddEvenSubarray.java
@@ -2,8 +2,9 @@ package com.varun;
 
 public class MamimumLenghtAlternateOddEvenSubarray {
     public static void main(String[] args) {
-        int []arr = {10,12,14,7,8};
-        int ans =maxLength(arr);
+//        int []arr = {10,12,14,7,8};
+        int []arr = {5,6,20,6,3,8};
+        int ans =maxLength1(arr);
         System.out.println(ans);
     }
 
@@ -27,6 +28,21 @@ public class MamimumLenghtAlternateOddEvenSubarray {
         return maxcount;
     }
 
+    private static int maxLength1(int[] arr) {
+        //optimized approach
+        int max = 1;
+        int curr=1 ;
+        for (int j = 1; j <arr.length ; j++) {
+            if((arr[j]%2==0 && arr[j-1]%2==1) || (arr[j]%2==1 && arr[j-1]%2==0)){
+                curr++;
+            }
+            else{
+                curr=1;
+            }
+            max = Math.max(curr,max);
+        }
+        return max;
+    }
 
 
 }
