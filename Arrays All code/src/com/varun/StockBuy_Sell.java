82f6a4ac4643dbb873719b94db415e65946d9a7f
varunbgit <vbansalevents01@gmail.com>
2022-12-17 20:18:23 +0530

    Stock Buy and Sell efficent solution GFG

--- Arrays All code/src/com/varun/StockBuy_Sell.java
+++ Arrays All code/src/com/varun/StockBuy_Sell.java
@@ -1,7 +1,19 @@
 package com.varun;
-
+//this question is from gfg practice please do not compare it with leetcode , both are different
 public class StockBuy_Sell {
     public static void main(String[] args) {
+        int [] arr = {1,5,3,8,12};
+        int ans = maxProfit(arr);
+        System.out.println(ans);
+    }
 
+    public static int maxProfit(int[] arr){
+        int profit =0;
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i+1] >arr[i]){
+                 profit += arr[i+1]-arr[i];
+            }
+        }
+        return profit;
     }
 }
