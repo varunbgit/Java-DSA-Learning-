ba0247710646825d66ab3d991bb1936d383d9a13
varunbgit <vbansalevents01@gmail.com>
2022-12-18 12:26:26 +0530

    Maximum sum of SubArray of given lenght optimized and neat  V3

--- Arrays All code/src/com/varun/MaxSumOfConsecutiveKelements.java
+++ Arrays All code/src/com/varun/MaxSumOfConsecutiveKelements.java
@@ -2,13 +2,14 @@ package com.varun;
 
 public class MaxSumOfConsecutiveKelements {
     public static void main(String[] args) {
-//        int []arr = {1,8,30,-5,20,7};
-        int []arr = {10,5,-2,20,1};
-        int k =3;
+        int []arr = {1,8,30,-5,20,7};
+//        int []arr = {10,5,-2,20,1};
+        int k =4;
         int ansOp = maxSum1(arr,k);
         System.out.println(ansOp);
         int ansNa = maxSum(arr,k);
         System.out.println(ansNa);
+        System.out.println(maxSum2(arr,k));
     }
 
     private static int maxSum(int[] arr,int k) {
@@ -42,5 +43,17 @@ public class MaxSumOfConsecutiveKelements {
         }
         return maxSum;
     }
-
+    private static int maxSum2(int[] arr,int k) {
+        int maxSum=Integer.MIN_VALUE;
+        int sum =0;
+        for (int i = 0; i <k ; i++) {
+            sum+=arr[i];
+        }
+        maxSum = sum;
+        for (int i = k; i < arr.length ; i++) {
+            sum = sum + arr[i] -arr[i-k];
+            maxSum = Math.max(maxSum,sum);
+        }
+        return maxSum;
+    }
 }
