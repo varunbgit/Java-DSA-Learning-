a5d7f78c02d8aa8e8c88cefa3b2ba00f4ee7a46b
varunbgit <vbansalevents01@gmail.com>
2022-12-21 12:35:30 +0530

    Added a Driver code for tree data Structure

--- Trees/src/com/varun/LevelOrderTraversal.java
+++ Trees/src/com/varun/LevelOrderTraversal.java
@@ -4,7 +4,7 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class LevelOrderTraversal {
-    public  class Node
+    public  static class Node
     {
         int data;
         Node left, right;
@@ -14,6 +14,18 @@ public class LevelOrderTraversal {
             data = item;
             left = right = null;
         }
+    }
+    public static void main(String args[])
+    {
+        Node root=new Node(10);
+        root.left=new Node(20);
+        root.right=new Node(30);
+        root.left.left=new Node(40);
+        root.left.right=new Node(50);
+        root.right.left=new Node(60);
+        root.right.right=new Node(70);
+
+        levelOrder(root);
     }
 
 
