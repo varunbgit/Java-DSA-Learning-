8b00ecc37fac700c968072a76b2e396026d37e24
varunbgit <vbansalevents01@gmail.com>
2022-12-21 13:18:00 +0530

    Find Max value in the Given Binary Tree

--- Trees/src/com/varun/MaxmimumValInTree.java
+++ Trees/src/com/varun/MaxmimumValInTree.java
@@ -1,13 +1,39 @@
-//package com.varun;
-//
-//public class MaxmimumValInTree {
-//    int max = 0;
-//    Maxval(root,max);
-//    public int MaxVal(TreeNode root,int max){
-//        if(root==null){
-//            return Integer.MIN_VALUE;
-//        }
-//        max = Math.max(root.max,Math.max(MaxVal(root.left),MaxVal(root.right)));
-//        return ans;
-//    }
-//}
+package com.varun;
+
+public class MaxmimumValInTree {
+   static class Node
+    {
+        int key;
+        Node left;
+        Node right;
+        Node(int k){
+            key=k;
+            left=right=null;
+        }
+    }
+
+
+
+
+        public static void main(String args[])
+        {
+            Node root=new Node(20);
+            root.left=new Node(80);
+            root.right=new Node(30);
+            root.right.left=new Node(40);
+            root.right.right=new Node(50);
+
+            System.out.print(getMax(root));
+        }
+
+    private static int getMax(Node root) {
+        if(root==null){
+            return Integer.MIN_VALUE;
+        }
+        int maxVal = root.key;
+        maxVal=Math.max(root.left.key,root.right.key);
+        return maxVal;
+    }
+
+
+}
