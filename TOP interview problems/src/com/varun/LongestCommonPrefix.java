4f088165d28f7c9df8c3a0c78f44366db84a5ef6
varunbgit <vbansalevents01@gmail.com>
2022-12-27 11:16:14 +0530

    Longest Common Subsequnece

---
+++ TOP interview problems/src/com/varun/LongestCommonPrefix.java
@@ -0,0 +1,31 @@
+package com.varun;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String [] str  = {"ab", "a"};
+        String ans = longestCommonPrefix(str);
+        System.out.println(ans);
+    }
+    public static  String longestCommonPrefix(String[] strs) {
+
+        String ans= "";
+        if(strs.length < 2){
+            return strs[0];
+        }
+
+        for (int i = 0; i <strs[0].length() ; i++) {
+            char temp = 0;
+            if(i<strs[0].length())
+                 temp  = strs[0].charAt(i);
+
+            for(String s : strs){
+
+                if(i>s.length()-1 || s.charAt(i) != temp){
+                    return ans;
+                }
+            }
+            ans += temp;
+        }
+        return ans;
+    }
+}
