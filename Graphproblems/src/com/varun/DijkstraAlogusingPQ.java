f2cbf01344ae90bfc0c617d6ef0272855ce11867
varunbgit <vbansalevents01@gmail.com>
2022-12-29 23:42:39 +0530

    Dijikstra's Algo using Priority Queue implementation

---
+++ Graphproblems/src/com/varun/DijkstraAlogusingPQ.java
@@ -0,0 +1,21 @@
+package com.varun;
+
+import java.util.*;
+
+public class DijkstraAlogusingPQ {
+    public static class Pair{
+        int distance;
+        int node;
+
+        Pair(int distance,int node){
+            this.distance = distance;
+            this.node = node;
+        }
+    }
+    public static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    {
+
+
+
+    }
+}
