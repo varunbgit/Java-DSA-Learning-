17a1dc1c8370c66baa2f8b13a099f3b2cad83868
varunbgit <vbansalevents01@gmail.com>
2022-12-30 19:57:25 +0530

    Bellman ford Algorithum

---
+++ Graphs/src/com/varun/BellmanFordAlgo.java
@@ -0,0 +1,35 @@
+package com.varun;
+//https://practice.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=distance-from-the-source-bellman-ford-algorithm
+
+
+import java.util.*;
+
+public class BellmanFordAlgo {
+
+    static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> edges, int S) {
+
+        // Write your code here
+        int[] distance = new int[V];
+        Arrays.fill(distance,(int)(1e8));
+        distance [S] = 0;
+
+        for (int i = 0; i <=V; i++) {
+            for (ArrayList<Integer> list : edges) {
+                int src = list.get(0);
+                int destt = list.get(1);
+                int wt = list.get(2);
+                if (distance[u]!=(int )(1e8) && distance[destt] > distance[src] + wt) {
+
+                    if((distance[src]!= (int)(1e9)) && (i == V))
+                        return new int[]{-1}; // detecting a negative cycle
+
+
+                    distance[destt] = distance[src] + wt ;
+
+                }
+            }
+        }
+        return distance;
+
+    }
+}
