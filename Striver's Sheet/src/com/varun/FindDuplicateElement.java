a88f01936e952e59201bd4a9629d49928f727daf
varunbgit <vbansalevents01@gmail.com>
2023-01-06 14:06:01 +0530

    Find Duplicate element using Hashing

--- Striver's Sheet/src/com/varun/FindDuplicateElement.java
+++ Striver's Sheet/src/com/varun/FindDuplicateElement.java
@@ -1,12 +1,15 @@
 package com.varun;
 //https://leetcode.com/problems/find-the-duplicate-number/description/
 
+import java.util.HashSet;
+
 public class FindDuplicateElement {
 
     public static void main(String[] args) {
-//        int []nums = {1,3,4,2,2};
-        int []nums = {3,1,3,4,2};
-        int ans = findDuplicate(nums);
+        int []nums = {1,3,4,2,2};
+//        int []nums = {3,1,3,4,2};
+        int ans = findDuplicate2(nums);
+//                findDuplicate(nums);
         System.out.println(ans);
 }
 
@@ -29,5 +32,18 @@ public class FindDuplicateElement {
         int temp = nums[i];
         nums[i] = nums[num];
         nums[num] = temp;
+    }
+
+
+    public static int findDuplicate2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int ele : nums){
+            if(!set.contains(ele)){
+                set.add(ele);
+            }else{
+                return ele;
+            }
+        }
+        return 0;
     }
 }
