fc8b6c1735d37e4de5603522dd99cc07e73b9862
varunbgit <vbansalevents01@gmail.com>
2023-01-09 23:01:20 +0530

    starting with Programming Pathsahala Codes Here

--- Striver's Sheet/src/com/varun/UniquePaths.java
+++ Striver's Sheet/src/com/varun/UniquePaths.java
@@ -3,9 +3,9 @@ package com.varun;
 //https://leetcode.com/problems/unique-paths/
 public class UniquePaths {
     public static void main(String[] args) {
-        int m = 23;
-        int n =12 ;
-        int ans = uniquePaths2(m,n) ;
+        int m = 3;
+        int n =2 ;
+        int ans = uniquePaths3(m,n) ;
         System.out.println(ans);
     }
 
@@ -40,11 +40,42 @@ public class UniquePaths {
 
 
     }
+    //optimized approach
+    //     using memorizing technique
+    public static int uniquePaths3(int m, int n) {
+        int[][] noPaths = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                noPaths[i][j] = -1;
+            }
+        }
+//        noPaths[0][0] = paths(m,n,noPaths);
 
-//    public static int uniquePaths(int m, int n) {
-//
-//
-//    }
+        return paths(0,0,noPaths);
+    }
+    public static int paths(int sRow, int nCol , int[][] noPaths){
+        int m = noPaths.length;
+        int n = noPaths[0].length;
+
+        if(sRow >= m || nCol >= n){
+            return 0;
+        }
+        if(sRow == m-1 && nCol == n-1){
+            return 1;
+        }
+
+        if(noPaths[sRow][nCol] != -1){
+            return noPaths[sRow][nCol];
+        }
+
+//        noPaths[sRow][nCol] += noPaths[sRow+1][nCol]  + noPaths[sRow][nCol + 1];
+
+        return noPaths[sRow][nCol] =
+                paths(sRow +1,nCol,noPaths) +
+                paths(sRow ,nCol+1,noPaths) ;
+
+
+    }
 
 
     // most optimized solution
