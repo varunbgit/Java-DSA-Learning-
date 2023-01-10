f39356f5f93a480a219407a27180e7ce0bdaafdd
varunbgit <vbansalevents01@gmail.com>
2023-01-10 21:40:42 +0530

    Find Unique Subset of a given Array

--- PPathShala codeas/src/com/varun/Subsets.java
+++ PPathShala codeas/src/com/varun/Subsets.java
@@ -1,27 +1,30 @@
 package com.varun;
-import java.util.*;
-public class Main {
 
+//https://leetcode.com/problems/subsets/
 
+import java.util.ArrayList;
+import java.util.List;
 
-        public static void main(String[] args) {
-            int[] nums = {1, 2, 3};
-            List<List<Integer>> ans = subsets(nums);
-            for (List<Integer> ls : ans) {
-                System.out.println(ls.toString());
-            }
-
+public class Subsets {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        List<List<Integer>> ans = subsets(nums);
+        for (List<Integer> ls : ans){
+            System.out.println(ls.toString());
         }
+    }
+
+
         static List<List<Integer>> result;
         public static List<List<Integer>> subsets(int[] nums) {
             result = new ArrayList();
             if(nums==null || nums.length==0) return result;
 
-            subsets(nums,new ArrayList<Integer>(), 0);
+            findsubsets(nums,new ArrayList<Integer>(), 0);
             return result;
         }
 
-        private static void subsets(int[] nums, ArrayList<Integer> temp, int index) {
+        public static void findsubsets(int[] nums, ArrayList<Integer> temp, int index) {
             // base condition
             if(index >= nums.length) {
                 result.add(new ArrayList<>(temp));
@@ -31,15 +34,12 @@ public class Main {
             // main logic
             // case 1 : we pick the element
             temp.add(nums[index]);
-            subsets(nums, temp, index+1); // move ahead
+            findsubsets(nums, temp, index+1); // move ahead
             temp.remove(temp.size()-1);
 
             // case 2 : we don't pick the element ( notice, we did not add the current element in our temporary list
-            subsets(nums, temp, index+1); // move ahead
-
-
-
-
+            findsubsets(nums, temp,index+1); // move ahead
         }
-    }
 
+
+}
